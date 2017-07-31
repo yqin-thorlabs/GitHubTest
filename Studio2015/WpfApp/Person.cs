cffb24c1dd364a7bf2fedb507a1c1ff860d1f4b8
yqin <yqin99@yahoo.com>
2017-07-30 22:34:25 -0500

    add unit test

---
+++ Studio2015/WpfApp/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
